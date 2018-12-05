e0f207cd8842363c2b41e87ec56b2628b393cbb5
Rohit Ramesh <rohit507@gmail.com>
2018-12-04 20:40:31 -0800

    Fixxed bug in Day4 part2

---
+++ src/Day5.hs
@@ -0,0 +1,23 @@
+module Day5 where
+
+
+import Text.Parsec
+import Text.Parsec.Char
+import Text.Parsec.Combinator
+import Text.Parsec.Prim
+import Text.Parsec.String
+
+import Debug.Trace
+
+import Text.Show.Pretty
+
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+
+import Data.List
+
+part1 :: IO ()
+part1 = undefined
+
+part2 :: IO ()
+part2 = undefined
--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,7 @@ import qualified Day1
 import qualified Day2
 import qualified Day3
 import qualified Day4
+import qualified Day5
 
 main :: IO ()
-main = Day4.part2
+main = Day5.part1
